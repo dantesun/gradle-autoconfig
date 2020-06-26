ea8458149713483929afc0446eba0c6bbd522b03
Dante Sun <dante.sun@gmail.com>
2020-06-27 02:49:38 +0800

    1. try com.palantir.git-version. 2. Upgrade gradle wrapper to 4.2.1

--- buildSrc/src/main/groovy/autoconfig.groovy
+++ buildSrc/src/main/groovy/autoconfig.groovy
@@ -1,3 +1,5 @@
 class PluginInfo {
     public static final AXION_RELEASE = 'pl.allegro.tech.build.axion-release'
+
+    public static final String PALANTIR_GIT_VERSION = "com.palantir.git-version"
 }
